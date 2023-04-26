e29257a36404782d057c6a66bfe64b68f6555e7b
Rohan Harnale <rohanharnale@M-FVFJMT2Q1WFV.local>
2023-04-26 12:28:51 +0530

    first commit

---
+++ dist/db/dbConfig.js
@@ -0,0 +1,12 @@
+// const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/node-file-upl', { useNewUrlParser: true });
+const conn = mongoose.connection;
+conn.on('connected', function () {
+    console.log('database is connected successfully');
+});
+conn.on('disconnected', function () {
+    console.log('database is disconnected successfully');
+});
+conn.on('error', console.error.bind(console, 'connection error:'));
+module.exports = conn;
+//# sourceMappingURL=dbConfig.js.map
---
+++ src/app.ts
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require('path');
+const fs = require("fs");
+const multer = require("multer");
+const imageModel = require('../src/models/imageModel.js');
+const mongoose = require("mongoose");
+ 
+ 
+app.use(bodyParser.urlencoded(
+      { extended:true }
+))
+
+// SET STORAGE
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      console.log("file", file);
+      
+      cb(null, Date.now())
+    }
+  })
+ 
+const upload = multer({ dest: "uploads" })
+ 
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+ 
+app.post("/uploadphoto",upload.single('myImage'),(req,res)=>{  
+    var img = fs.readFileSync(req.file.path);
+    var encode_img = img.toString('base64');
+    var final_img = {
+        contentType:req.file.mimetype,
+        image:new Buffer(encode_img,'base64')
+    };
+    imageModel.create(final_img,function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(result.img.Buffer);
+            console.log("Saved To database");
+            res.contentType(final_img.contentType);
+            res.send(final_img.image);
+        }
+    })
+})
+
+app.get("/ping", (req, res) => {
+    res.json({
+        status: 200,
+        message: "Pong"
+    })
+})
+//Code to start server
+app.listen(8080,function () {
+      console.log("Server Started at PORT 8080");
+})
